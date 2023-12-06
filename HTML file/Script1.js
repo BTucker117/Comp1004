d3dd0a4307a0228c172fa243ee035970a94a56c9
Ben Tucker <146118634+BTucker117@users.noreply.github.com>
2023-12-06 11:02:18 +0000

    External Log in box - Fail
    Tried to program an external log in box so user info can be displayed when the correct credentials are entered. This didn't work

--- HTML file/Script1.js
+++ HTML file/Script1.js
@@ -6,3 +6,12 @@ function changeText(id) {
 function displayDate(id) {
     id.innerHTML = "Date is Today";
 }
+
+
+document.getElementById("button").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex";
+});
+
+document.getElementById(".close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
--- Sahara/Sahara Script.js
+++ Sahara/Sahara Script.js
@@ -1,1 +1,9 @@
 // JavaScript source code
+
+document.getElementById("button").addEventListener("click", function (){
+    document.querySelector(".popout").style.display = "flex";
+});
+
+document.getElementById(".close").addEventListener("click", function () {
+    document.querySelector(".popout").style.display = "none";
+});
