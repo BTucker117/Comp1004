a4ddf220205e8750c7baf7db217f32de897cd629
Ben Tucker <146118634+BTucker117@users.noreply.github.com>
2024-03-20 12:32:33 +0000

    Comment here
    Added many comments throughout the JavaScript code

--- Sahara/Sahara Script.js
+++ Sahara/Sahara Script.js
@@ -2,18 +2,18 @@
 
 
 
-console.log("Hello ?");
+console.log("Hello ?"); //Test
 
 var openb = document.getElementById("open");
 
 var box = document.getElementById("box");
 
-function open_box() {
+function open_box() { //Function to open the box
     box.style.display = "block";
     openb.style.display = "none";
 }
 
-function close_box() {
+function close_box() { //Function to close the box
     box.style.display = "none";
     openb.style.display = "block";
 }
@@ -22,7 +22,7 @@ Username = null; Password = null;
 function Input() {
     Username = document.getElementById("Username").value;
     Password = document.getElementById("Password").value;
-    console.log("Username: ", Username, "Password: ", Password);
+    console.log("Username: ", Username, "Password: ", Password); //Test
 };
 
 /*
@@ -85,10 +85,10 @@ setInterval(function () {
 }, 1000);
 */
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function() { //NO LONGER IN USE
 
     const doc = document.getElementById("Get_File")
-    console.log("Hallo from before the .Json code, Lets hope we go in")
+    console.log("Hallo from before the .Json code, Lets hope we go in") //Test
     if (doc) {
         doc.addEventListener("change", function () { console.log("Hallo ?!") /* THIS IS WHERE ISSUES OCCOUR. WE DON'T ENTER THIS FUNCTION */
             var read = document.getElementById("Get_File").files[0]; //Outline the file to be read
@@ -103,16 +103,16 @@ window.addEventListener("DOMContentLoaded", function() {
         });
 
     };
-    console.log("Hallo from the end if statement. Do you see this ?")
+    console.log("Hallo from the end if statement. Do you see this ?") //Test
 
 });
-console.log("Hallo from after the .Json, did we go into the code?")
+console.log("Hallo from after the .Json, did we go into the code?") //Test
 
 
-function Opened() {
+function Opened() { // Paswword generator
     javascript: (
         function Gener() {
-            prompt('Your Secure Password:',
+            prompt('Your Secure Password:', //Pull up a browser promtp
                 window.crypto.getRandomValues(new BigUint64Array(4)).reduce(
                     (prev, curr, index) => (
                         !index ? prev : prev.toString(36)
@@ -120,33 +120,33 @@ function Opened() {
                             index % 2 ? curr.toString(36).toUpperCase() : curr.toString(36)
                         )
                 ).split('').sort(() => 128 -
-                    window.crypto.getRandomValues(new Uint8Array(1))[0]
+                    window.crypto.getRandomValues(new Uint8Array(1))[0] // Get the random values for the password
                 ).join('')
             );
         }
     )();
 };  
 
-console.log("Hallo, if you are reading this then it means the Java works and gets to this point");
+console.log("Hallo, if you are reading this then it means the Java works and gets to this point"); //Test
 
 
 //Basic inner .Json
 
-const Admin_account = {Username:"Admin", Password: "Admin1", Type: "Superuser", ID: "000"}
-console.log(Admin_account.Username, ":", Admin_account.Password, ":", Admin_account.Type, ":", Admin_account.ID);
+const Admin_account = {Username:"Admin", Password: "Admin1", Type: "Superuser", ID: "000"} //Inbedded admin credentials
+console.log(Admin_account.Username, ":", Admin_account.Password, ":", Admin_account.Type, ":", Admin_account.ID); //Test
 
-account = {Username: [], Password: [], Type: [], ID: []};
+account = {Username: [], Password: [], Type: [], ID: []}; //Empty Json template for data to be put into
 console.log(account.Username[1], ":", account.Password[1], ":", account.Type[1], ":", account.ID[1]); //Test
 New_ID = null;
 ID();
 Account_Input();
 
-console.log(New_ID);
+console.log(New_ID); //Test
 New_Acc = 0;
 
 function ID() {
-    console.log("HAllo ?!");
-    Temp_ID_Ad = 0; Temp_ID_Norm = null;
+    console.log("HAllo ?!"); //Test
+    Temp_ID_Ad = 0; Temp_ID_Norm = null; //Initialise data
     Temp_string_Ad = JSON.stringify(Admin_account.ID);
     Temp_string_Norm = JSON.stringify(account.ID);
 
@@ -155,7 +155,7 @@ function ID() {
     } catch {
         Temp_ID_Ad = 0;
     }
-    console.log(Temp_ID_Ad, "New ID would be", Temp_ID_Ad + 1);
+    console.log(Temp_ID_Ad, "New ID would be", Temp_ID_Ad + 1); //Test and see what next ID would be
 
     try {
         Temp_ID_Norm = Interger.parseInt(Temp_string_Norm)
@@ -163,19 +163,19 @@ function ID() {
         Temp_ID_Norm = 1;
     }
 
-    console.log("Hallo Do we get ere ? Norm is", Temp_ID_Norm);
-    if (Temp_ID_Norm == null)
-        New_ID = 1;
+    console.log("Hallo Do we get ere ? Norm is", Temp_ID_Norm); //Test
+    if (Temp_ID_Norm == null) //If no accounts then make new with ID of 1
+        New_ID = 1; 
 
-    else if (Temp_ID_Norm =! null)
+    else if (Temp_ID_Norm =! null) //If there is already an account(s) then give ID of current highest +1
         New_ID = Temp_ID_Norm + 1;
 }
 
 function Account_Input() { //New Input function
     
-    Username_new = document.getElementById("Username").value;
-    Password_new = document.getElementById("Password").value;
-    New_Acc = New_ID + 1;
+    Username_new = document.getElementById("Username").value; //Get username input
+    Password_new = document.getElementById("Password").value; //Get Password input
+    New_Acc = New_ID + 1; //Create an initialiser fot the new account to be stored under
 
     account.Username[New_Acc] = Username_new;
     account.Password[New_Acc] = Password_new;
@@ -204,14 +204,14 @@ function Hide(){ // A function to hide all the current elements on the screen
         x.style.display = "block";
     }
 
-    var y = document.getElementById("Return");
+    var y = document.getElementById("Return"); // A function to return back to the home screen
     if (y.style.display === "none") {
         y.style.display = "block";
     } else {
         y.style.display = "none";
     }
 
-    var y = document.getElementById("Hidden");
+    var y = document.getElementById("Hidden"); // A function to hide log in objects until users log in
     if (y.style.display === "none") {
         y.style.display = "block";
     } else {
@@ -238,17 +238,17 @@ function Change_date() { // Function to add 3 months to the current date
 //EXPERIMANTATION//
 
 function convertToJSON() {
-    var username = document.getElementById('Username').value;
-    var password = document.getElementById('Password').value;
+    var username = document.getElementById('Username').value; //Get usernmae input
+    var password = document.getElementById('Password').value; //Get password input
   
-    var jsonObject = {
+    var jsonObject = { //internal Json
       "UserName": username,
       "Password": password,
       "User Type": "Standard",
       "Accounnt ID": New_Acc,
     }
   
-    document.getElementById('subbutton').value = JSON.stringify(jsonObject)
+    document.getElementById('subbutton').value = JSON.stringify(jsonObject) //Convert when log-in button pressed
 }
   
 
@@ -260,14 +260,14 @@ function saveToFile() {
       //type: 'application/json' //Testing
       type: 'octet/stream'
     });
-    console.log(blob);
+    console.log(blob); //Test
   
     var anchor = document.createElement('a')
     anchor.download = "Sahara_user.json"; // Downloads the username and password entered on the button press
     anchor.href = window.URL.createObjectURL(blob);
     anchor.click();
   
-    console.log(anchor); //Out put the anchor to the log for testing purposes only
+    console.log(anchor); //Output the anchor to the log for testing purposes only
   
     document.getElementById('subbutton').append(anchor)
 }
@@ -285,7 +285,7 @@ function Add() {
     var dupe = first.cloneNode('true'); // Clones the div
     dupe.id = 'account_box_container' + z; // Gives a new ID of the original +1
     first.parentNode.appendChild(dupe);
-    console.log('Function Test: Do we get here log #01')
+    console.log('Function Test: Do we get here log #01') //Test
 }
 
 /* Saving a JSON file Locally */
@@ -303,14 +303,14 @@ function saveToLocal(){
 var load;
 saveToLocal();
 loadLocal();
-console.log("Hallo ? do we get to here ???");
-function loadLocal (){
+console.log("Hallo ? do we get to here ???"); //Test
+function loadLocal (){ //Load Json file from internal storage
     load = JSON.parse(localStorage.getItem("Sahara_Users")); //Load the item 'Sahara_Users' from local storage
     console.log(load); //Output the content from var load to console as a test
     ThisAccount(); 
 }
 
-function ThisAccount() {
+function ThisAccount() { // A function to copy Json contents to the HTML page
     console.log(load); // Test to ensure load is not empty and can be accessed
     document.getElementById("This_Account").innerHTML=load; //Copy the content of 'load' to the HTML doc
     console.log("HI"); // Test to ensure the code gets to this point and the previous line has run
